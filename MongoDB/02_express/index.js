317e944b38ad553b56431dcba177032156a2ed41
Rishav Kumar <rishavkumar18902@gmail.com>
2024-11-14 00:26:38 +0530

    CRUD operations

--- MongoDB/02_express/index.js
+++ MongoDB/02_express/index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 let teaData = [];
 let nextId = 1;
 
+// create
+
 app.post('/teas', (req, res) => {
   const { name, price } = req.body;
   const newTea = { id: nextId++, name, price };
@@ -19,9 +21,15 @@ app.post('/teas', (req, res) => {
   console.log(newTea)
 });
 
+
+// read
+
 app.get('/teas', (req, res) => {
     res.status(200).send(teaData)
 })
+
+
+// update 
 
 app.put('/teas/:id', (req, res) => {
   const tea = teaData.find(t => t.id === parseInt(req.params.id));
@@ -32,8 +40,21 @@ app.put('/teas/:id', (req, res) => {
   const { name, price } = req.body
   tea.name = name
   tea.price = price
-  res.send(200).send()
+  res.send(200).send(tea)
 })
+
+
+// delete 
+
+app.delete('/teas/:id', (req, res) => {
+  const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
+  if (index === -1) {
+    return res.status(404).send('Error not found')
+  }
+  teaData.splice(index, 1);
+  return res.status(204).send('Deleted')
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port}...`);
