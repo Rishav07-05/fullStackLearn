869086251d8d8ec77cab9e59f16224d054de697b
Rishav Kumar <rishavkumar18902@gmail.com>
2024-11-17 13:22:00 +0530

    Knowing about healthcheck

--- MongoDB/03_vidTube/src/app.js
+++ MongoDB/03_vidTube/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from "cors"
+import healthCheckRouter from './routes/healthCheck_Route.js'
 
 const app = express()
 
@@ -18,5 +19,8 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 
+//routes 
+
+app.use("/api/v1/healthCheck" , healthCheckRouter)
 
 export { app }
