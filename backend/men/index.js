9d7530941b08f5bae9d6a746fd98011baa948bd7
Rishav Kumar <rishavkumar18902@gmail.com>
2024-12-28 10:38:48 +0530

    Idea about req and res

--- backend/men/index.js
+++ backend/men/index.js
@@ -1,10 +1,22 @@
 // const catMe = require('cat-me')
 // console.log(catMe());
 
+const { log } = require('console')
 const http = require('http')
 
-const server = http.createServer((req , res) => {
-    res.end('Server created successfully')
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Home Page')
+    }
+
+    if (req.url === '/about') {
+        res.end('About Page')
+    }
+
+    if (req.url === '/profile') {
+        res.end("The Profile Page")
+    }
+
 })
 
 server.listen(3000)
