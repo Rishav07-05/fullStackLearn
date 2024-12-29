dbe14be53171a706f906d2a73168d9475b9d01bf
Rishav Kumar <rishavkumar18902@gmail.com>
2024-12-29 21:36:57 +0530

    Learn about EJS

--- backend/men/index.js
+++ backend/men/index.js
@@ -1,22 +1,71 @@
+// About http and req,res argument
+
 // const catMe = require('cat-me')
 // console.log(catMe());
 
-const { log } = require('console')
-const http = require('http')
+// const { log } = require('console')
+// const http = require('http')
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('Home Page')
-    }
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.end('Home Page')
+//     }
 
-    if (req.url === '/about') {
-        res.end('About Page')
-    }
+//     if (req.url === '/about') {
+//         res.end('About Page')
+//     }
 
-    if (req.url === '/profile') {
-        res.end("The Profile Page")
-    }
+//     if (req.url === '/profile') {
+//         res.end("The Profile Page")
+//     }
 
+// })
+
+// server.listen(3000)
+
+
+
+
+// express for production grade work 
+// Get method && setting rendering engine -> EJS && middlewares
+
+
+const express = require('express')
+const morgan = require('morgan')
+const app = express();
+app.use(morgan('dev'))
+
+
+app.set('view engine', 'ejs')
+
+
+// Custom middleware
+
+app.use((req, res, next) => {
+    console.log('This are middlewares');
+    const a = 2, b = 2;
+    console.log(a + b)
+    return next();
 })
 
-server.listen(3000)
+// Third Party middleware -> Morgan (Request Logger)
+
+
+
+
+//GET method
+
+app.get('/', (req, res, next) => {
+    const a = 5, b = 5;
+    console.log(a + b);
+    return next();
+},(req, res) => {
+    res.render('index')
+})
+
+
+app.get('/about', (req, res) => {
+    res.send('About Page')
+})
+
+app.listen(3000);
